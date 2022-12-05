ed182f9a4a460bd0ea2cee0a0938b2ad510493f5
Bőti Marcell <de9d6m@inf.elte.hu>
2022-12-05 15:03:00 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "..", "build");
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
